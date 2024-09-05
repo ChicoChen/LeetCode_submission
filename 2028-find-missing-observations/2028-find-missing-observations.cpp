1fc21e871c4a77203d65538fef8529b6362ff79f
ChicoChen <107322822+ChicoChen@users.noreply.github.com>
2024-09-06 01:22:43 +0800

    Time: 117 ms (5.88%), Space: 116.4 MB (69.12%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+          
+//take 1-greedyalgorithm: push_back maximun number answer[i] can have
+//take 2: learn from editorial
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         int m = rolls.size();
         int m_sum = 0;
         for(int i= 0; i< m; i++) m_sum += rolls[i];
         int n_sum = mean * (n + m) - m_sum;
 
-        vector<int> answer;
-        if(n_sum < n || n_sum > n * 6) return answer;
+        if(n_sum < n || n_sum > n * 6) return vector<int>();
 
-        for(int i= 1; i<= n; i++){
-            //greedyly: push_back maximun number answer[i] can have
-            answer.push_back(min(6, n_sum - (n - i)));
-            n_sum -= answer.back();
+        int n_mean = n_sum / n;
+        int n_remain = n_sum % n;
+        vector<int> answer(n, n_mean);
+        for(int i = 0; i< n_remain; i++){
+            answer[i]++;
         }
 
         return answer;
