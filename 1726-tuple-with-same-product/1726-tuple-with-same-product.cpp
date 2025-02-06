6d4320c369863bc2ba5dccbe54317a799ee0541b
ChicoChen <107322822+ChicoChen@users.noreply.github.com>
2025-02-07 00:58:34 +0800

    Time: 370 ms (21.63%), Space: 85.1 MB (23.2%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -3,17 +3,14 @@ public:
     //I referred the hint
     int tupleSameProduct(vector<int>& nums) {
         int n = nums.size();
-        //std::sort(nums.begin(), nums.end());
         unordered_map<int, int> count;
+        int result = 0;
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
-                count[nums[i] * nums[j]]++;
+                int product = nums[i] * nums[j];
+                result += count[product] * 8;
+                count[product]++;
             }
-        }
-
-        int result = 0;
-        for(auto cnt: count){
-            result += cnt.second * (cnt.second - 1) * 4;
         }
 
         return result;
